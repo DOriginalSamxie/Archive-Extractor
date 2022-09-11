6497ddd52886fd035e92644fe6c2b48745ba83af
DOriginalSamxie <samadrufai@gmail.com>
2022-09-11 16:15:29 +0100

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
-document.querySelector('#open_file_button').addEventListener('click', check)
-function check() {
-  const hiddenInput = document.querySelector('#input-file')
-}
+// document.querySelector('#open_file_button').addEventListener('click', check)
+// function check() {
+//   const hiddenInput = document.querySelector('#input-file')
+// }
 // let file = new File(...FileSystem)
+'use strict'
 
-// // Open the file archive for reading
-// Unarchiver.open(file).then(async function (archive) {
-//   for (let entry of archive.entries) {
-//     if (entry.is_file) {
-//       // File object for archive entry
-//       let entry_file = await entry.read()
-//     }
-//   }
-// })
+var $result = $('#result')
+$('#file').on('change', function (evt) {
+  // remove content
+  $result.html('')
+  // be sure to show the results
+  $('#result_block').removeClass('hidden').addClass('show')
+
+  // Closure to capture the file information.
+  function handleFile(f) {
+    var $title = $('<h4>', {
+      text: f.name,
+    })
+    var $fileContent = $('<ul>')
+    $result.append($title)
+    $result.append($fileContent)
+
+    var dateBefore = new Date()
+    JSZip.loadAsync(f) // 1) read the Blob
+      .then(
+        function (zip) {
+          var dateAfter = new Date()
+          $title.append(
+            $('<span>', {
+              class: 'small',
+              text: ' (loaded in ' + (dateAfter - dateBefore) + 'ms)',
+            })
+          )
+
+          zip.forEach(function (relativePath, zipEntry) {
+            // 2) print entries
+            $fileContent.append(
+              $('<li>', {
+                text: zipEntry.name,
+              })
+            )
+          })
+        },
+        function (e) {
+          $result.append(
+            $('<div>', {
+              class: 'alert alert-danger',
+              text: 'Error reading ' + f.name + ': ' + e.message,
+            })
+          )
+        }
+      )
+  }
+
+  var files = evt.target.files
+  for (var i = 0; i < files.length; i++) {
+    handleFile(files[i])
+  }
+})
